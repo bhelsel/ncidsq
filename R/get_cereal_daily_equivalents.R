570a00c1bd0bd8d131a1dd037d663532015a3af3
Brian Helsel <bhelsel@kumc.edu>
2025-03-13 14:59:08 -0500

    Set up cereal conversion check for readr::read_csv

--- R/get_cereal_daily_equivalents.R
+++ R/get_cereal_daily_equivalents.R
@@ -18,15 +18,15 @@ get_cereal_daily_equivalents <- function(data, cereal_names = c("cereal_type1", 
 
   for(v in 1:length(cereal_names)){
     data[[cereal_names[v]]] <- gsub("\u00D5","'", iconv(data[[cereal_names[v]]], from = "ISO-8859-1", to = "UTF-8"))
-    assign(sprintf("n_%s", cereal_names[v]), length(data[data[[cereal_names[v]]] != "", cereal_names[v], drop = TRUE]))
+    assign(sprintf("n_%s", cereal_names[v]), length(data[data[[cereal_names[v]]] != "" & !is.na(data[[cereal_names[v]]]), cereal_names[v], drop = TRUE]))
     if(v == 1){
       data <- data %>% dplyr::left_join(calib_DSQ_cereal_ntile, by = dplyr::join_by(!!rlang::sym(cereal_names[v]) == Cereal_Name))
-      if(get(sprintf("n_%s", cereal_names[v])) != length(data[!is.na(data$food_code), "food_code", drop = TRUE])){
+      if(get(sprintf("n_%s", cereal_names[v]), envir = environment()) != length(data[!is.na(data$food_code), "food_code", drop = TRUE])){
         stop("All of the first cereal choices did not match with the dietary screening questionnaire")
       }
     } else if(v == 2){
       data <- data %>% dplyr::left_join(calib_DSQ_cereal_ntile, by = dplyr::join_by(!!rlang::sym(cereal_names[v]) == Cereal_Name), suffix = c("1", "2"))
-      if(get(sprintf("n_%s", cereal_names[v])) != length(data[!is.na(data$food_code2), "food_code2", drop = TRUE])){
+      if(get(sprintf("n_%s", cereal_names[v]), envir = environment()) != length(data[!is.na(data$food_code2), "food_code2", drop = TRUE])){
         stop("All of the second cereal choices did not match with the dietary screening questionnaire")
       }
     }
