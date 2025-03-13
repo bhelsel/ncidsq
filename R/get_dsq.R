8d9937a0b619f618374309b112e456857c1deb88
Brian Helsel <bhelsel@kumc.edu>
2025-03-13 14:59:34 -0500

    Move up age and rename_variable arguments to the main function

--- R/get_dsq.R
+++ R/get_dsq.R
@@ -1,20 +1,22 @@
 #' @title get_dsq
 #' @description FUNCTION_DESCRIPTION
 #' @param data PARAM_DESCRIPTION
+#' @param id PARAM_DESCRIPTION
+#' @param timepoint PARAM_DESCRIPTION
+#' @param birthdate PARAM_DESCRIPTION
+#' @param visitdate PARAM_DESCRIPTION
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @rdname get_dsq
 #' @export
 #' @importFrom dplyr `%>%`
 
-get_dsq <- function(data){
+get_dsq <- function(data, id = "ptid", timepoint = "redcap_event_name",
+                    birthdate = "subject_birthdate", visitdate = c("visityr", "visitmo", "visitday"),
+                    reference = "kumc"){
   data %>%
-    calculate_age_at_visit(
-      id = "ptid", timepoint = "redcap_event_name",
-      birthdate = "subject_birthdate",
-      visitdate = c("visityr", "visitmo", "visitday")
-    ) %>%
-    rename_variables() %>%
+    calculate_age_at_visit(id, timepoint, birthdate, visitdate) %>%
+    rename_variables(reference) %>%
     apply_factor_labels() %>%
     convert_to_daily_equivalents() %>%
     get_cereal_daily_equivalents() %>%
